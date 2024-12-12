cf3016a345d362d11ced8e3f87a0d5aa03cfbd1e
emayge001 <onuegbuoluwaseun@gmail.com>
2024-12-12 13:18:54 +0100

    next- dashboard

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const customers = () => {
+  return (
+    <>
+        <h1>This is the customers page</h1>
+    </>
+  )
+}
+
+export default customers
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const invoices = () => {
+  return (
+    <>
+        <h1>This is the invoices page</h1>
+    </>
+  )
+}
+
+export default invoices
